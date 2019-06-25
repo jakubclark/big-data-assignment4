8f9f3e8f38a9042cb54a1f5a15c8736eb2881ced
Kuba Clark <jakub.clark@protonmail.com>
2019-06-25 08:46:23 +0200

    Add plot_price_by_quadrant

--- big_data/plots.py
+++ big_data/plots.py
@@ -47,9 +47,49 @@ def plot_price_by_grade(df: pd.DataFrame):
     plt.show()
 
 
+def plot_price_by_quadrant(df: pd.DataFrame):
+    groups = df.groupby('QUADRANT')
+
+    means = {}
+
+    for quadrant, props in groups:
+        prices: pd.Series = props['PRICE']
+        prices = prices.dropna()
+        mean_price = sum(prices) / len(prices)
+        means[quadrant] = mean_price
+
+    quadrant_labels = ['NW', 'NE', 'SE', 'SW']
+    y_pos = np.arange(len(quadrant_labels))
+
+    mean_prices = [means[quadrant] for quadrant in quadrant_labels]
+
+    fig, ax = plt.subplots()
+
+    ax.barh(y_pos, mean_prices, align='center')
+
+    ax.set_axisbelow(True)
+    ax.grid(linestyle='-', linewidth='0.5', color='black', which='both')
+
+    minor_xticks = np.arange(0, 1500001, 100000)
+    major_xticks = np.arange(0, 1500001, 500000)
+    ax.set_xticks(major_xticks)
+    ax.set_xticks(minor_xticks, minor=True)
+
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(quadrant_labels)
+
+    ax.set_xlabel('Mean Price ($)')
+    ax.set_ylabel('Quadrant Label')
+    ax.set_title('Mean Price By Quadrant')
+
+    fig.savefig('mean_price_by_quadrant.pdf')
+    plt.show()
+
+
 def main():
     df = pd.read_csv('DC_Properties.csv')
     plot_price_by_grade(df)
+    plot_price_by_quadrant(df)
 
 
 if __name__ == '__main__':
