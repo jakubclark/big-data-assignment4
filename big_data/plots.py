749ba9fbc3190e03065a5052092f9ab8a2684564
Kuba Clark <jakub.clark@fireeye.com>
2019-06-25 17:06:23 +0200

    Add plot_price_over_time, plot_price_heatmap, plot_count_heatmap

--- big_data/plots.py
+++ big_data/plots.py
@@ -86,10 +86,120 @@ def plot_price_by_quadrant(df: pd.DataFrame):
     plt.show()
 
 
+def plot_price_over_time(df):
+    subset = df[['PRICE', 'SALEDATE']].dropna()
+
+    subset = subset.sort_values(by=['SALEDATE'])
+
+    year_to_prices = {}
+
+    for _, props in subset.iterrows():
+        saledate = props['SALEDATE']
+        price = props['PRICE']
+        year = int(saledate.split('-')[0])
+        if year in year_to_prices.keys():
+            year_to_prices[year].append(price)
+        else:
+            year_to_prices[year] = [price]
+
+    year_to_mean_prices = {}
+
+    for year, prices in year_to_prices.items():
+        mean_price = sum(prices) / len(prices)
+        year_to_mean_prices[year] = mean_price
+
+    x = list(year_to_mean_prices.keys())
+    y = list(year_to_mean_prices.values())
+
+    fig, ax = plt.subplots()
+
+    ax.plot(x, y)
+    ax.set_title('Year vs Mean Property Price')
+    ax.set_xlabel('Saledate')
+    ax.set_ylabel('Mean Price ($)')
+
+    fig.savefig('mean_saleprice_over_time.pdf')
+    plt.show()
+
+
+def plot_price_heatmap(df: pd.DataFrame):
+    def compute_color(price):
+        """
+        Go from price (a float) to a color (a string)
+        """
+        # TODO: Compute the color from a palette
+        if 0.0 < price < 500000.0:
+            return 'red'
+        elif 500000.0 < price < 1000000.0:
+            return 'blue'
+        elif 1000000.0 < price < 1500000:
+            return 'orange'
+        else:
+            return 'black'
+
+    subset = df[['LONGITUDE', 'LATITUDE', 'PRICE']].dropna()
+
+    data = {}
+
+    for _, props in subset.iterrows():
+        lon = props['LONGITUDE']
+        lat = props['LATITUDE']
+        value = props['PRICE']
+        key = (lon, lat)
+        if key in data.keys():
+            data[key].append(value)
+        else:
+            data[key] = [value]
+    x = []
+    y = []
+
+    c = []
+
+    for coords, prices in data.items():
+        lon, lat = coords
+        mean_price = sum(prices) / len(prices)
+        x.append(lon)
+        y.append(lat)
+        color = compute_color(mean_price)
+        c.append(color)
+
+    plt.clf()
+    plt.title('Mean Price map')
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+
+    plt.scatter(x, y, s=0.1, c=c)
+
+    plt.show()
+    plt.savefig('mean_price_by_coordinate.pdf')
+
+
+def plot_count_heatmap(df: pd.DataFrame):
+    subset = df[['LONGITUDE', 'LATITUDE']].dropna()
+    lons = subset['LONGITUDE']
+    lats = subset['LATITUDE']
+
+    heatmap, xedges, yedges = np.histogram2d(lons, lats, bins=(50, 50))
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+
+    plt.clf()
+    plt.title('Number of Properties Heatmap')
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+
+    plt.imshow(heatmap, extent=extent, origin='lower')
+
+    plt.show()
+    plt.savefig('number_of_properties_heatmap.pdf')
+
+
 def main():
     df = pd.read_csv('DC_Properties.csv')
     plot_price_by_grade(df)
     plot_price_by_quadrant(df)
+    plot_price_over_time(df)
+    plot_price_heatmap(df)
+    plot_count_heatmap(df)
 
 
 if __name__ == '__main__':
