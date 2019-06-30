aced87fab69479962621fa41cdc7d8e9b1332b28
Kuba Clark <jakub.clark@protonmail.com>
2019-06-30 18:21:55 +0200

    Update plots, update plots.py and add csv files

--- big_data/plots.py
+++ big_data/plots.py
@@ -1,3 +1,4 @@
+import matplotlib
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
@@ -6,9 +7,10 @@ from matplotlib.colors import LogNorm
 from scipy import stats
 from scipy.stats import pearsonr
 
-
 # NOTE: This file depends on a .csv file called `cpi_csv`, which is provided with the uploaded
 # Also, the plots are saved to a folder called `plots`, which will need to be created first
+
+matplotlib.rc('font', size=14)
 
 
 def plot_adjusted_price_over_time(df):
@@ -25,16 +27,16 @@ def plot_adjusted_price_over_time(df):
     cpi_df = pd.read_csv('cpi.csv', index_col='Year')
     current_cpi = 256.1
 
-    def make_plot(ignore_outliers=False):
-        print(f'Plotting mean sale price over time. ignore_outliers={ignore_outliers}')
+    def make_plot():
+        print(f'Plotting mean sale price over time.')
         subset = df[['PRICE', 'SALEDATE']].dropna()
 
         subset['YEAR'] = subset['SALEDATE'].apply(lambda date: int(date.split('-')[0]))
         groups = subset.groupby('YEAR')
 
-        final_df = pd.DataFrame(columns=['YEAR', 'MEAN_PRICE', 'ADJUSTED_MEAN_PRICE', 'MEAN_ADJUSTED_PRICE'])
+        final_df = pd.DataFrame(columns=['YEAR', 'MEAN_PRICE', 'ADJUSTED_MEAN_PRICE'])
         for year, props in groups:
-            if not ignore_outliers and year in (1991, 2007, 2008, 2014, 2015):
+            if year in (1991, 2007, 2008, 2014, 2015):
                 continue
             mean_price = props['PRICE'].mean()
             adjusted_mean_price = adjust_for_inflation(mean_price, year)
@@ -47,26 +49,22 @@ def plot_adjusted_price_over_time(df):
         plot = final_df.plot.line(
             x='YEAR',
             y=['ADJUSTED_MEAN_PRICE', 'MEAN_PRICE'],
+            figsize=(9, 5)
         )
 
         plot.minorticks_on()
         plot.set_axisbelow(True)
         plot.grid(linestyle='--', linewidth='0.5', color='black', which='major')
 
-        if ignore_outliers:
-            plot.set_title('Mean Price over Time - Without Outliers')
-        else:
-            plot.set_title('Mean Price over Time - With Outliers')
-
-        plot.set_xlabel('Year')
-        plot.set_ylabel('Price ($)')
+        plot.set_title('Mean Price over Time - Without Outliers', fontsize=12)
+        plot.set_xlabel('Year', fontsize=12)
+        plot.set_ylabel('Price ($)', fontsize=12)
 
         fig = plot.get_figure()
-        fig.savefig(f'plots/mean_price_over_time_{ignore_outliers}.pdf')
-        fig.savefig(f'plots/mean_price_over_time_{ignore_outliers}.png', dpi=300)
+        fig.savefig('plots/mean_price_over_time.pdf')
+        fig.savefig('plots/mean_price_over_time.png', dpi=300)
         plt.show()
 
-    make_plot(True)
     make_plot()
 
 
@@ -95,14 +93,15 @@ def plot_price_histogram(df):
     subset = df[['PRICE', 'QUALIFIED']].dropna().query('QUALIFIED == "Q"')
     prices = subset['PRICE']
 
-    hist = prices.hist(figsize=(8, 4))
+    hist = prices.hist(
+        figsize=(8, 5)
+    )
 
-    plt.title('Price Histogram')
-    plt.xlabel('Price ($)')
-    plt.ylabel('Count')
+    plt.title('Price Histogram', fontsize=12)
+    plt.xlabel('Price ($)', fontsize=12)
+    plt.ylabel('Count', fontsize=12)
 
     fig = hist.get_figure()
-
     fig.savefig('plots/price_histogram.pdf')
     fig.savefig('plots/price_histogram.png', dpi=300)
     plt.show()
@@ -117,7 +116,7 @@ def plot_boxplots_by_quadrant(df):
     for quadrant, props in groups:
         quads[quadrant] = props['PRICE']
 
-    fig, ax = plt.subplots(figsize=(9, 5))
+    fig, ax = plt.subplots()
 
     labels = ['NW', 'NE', 'SE', 'SW']
     prices = [quads['NW'], quads['NE'], quads['SE'], quads['SW']]
@@ -138,7 +137,7 @@ def plot_boxplots_by_quadrant(df):
 
     ax.set_ylabel('Quadrant')
     ax.set_xlabel('Price ($)')
-    ax.set_title('Price Boxplot by Quadrant')
+    ax.set_title('Price Boxplot by Quadrant', fontsize=12)
 
     plt.setp(bp['boxes'], color='blue', linewidth=1)
     plt.setp(bp['whiskers'], color='black', linewidth=1)
@@ -165,7 +164,7 @@ def plot_count_by_quadrant(df):
     x = ['NW', 'NE', 'SE', 'SW']
     y = [final_df.get_value(quad, 'Number of Properties') for quad in x]
 
-    fig, ax = plt.subplots(figsize=(9, 5))
+    fig, ax = plt.subplots()
 
     ax.barh(x, y, height=0.4)
 
@@ -173,13 +172,13 @@ def plot_count_by_quadrant(df):
     ax.grid(linestyle='--', linewidth='0.5', color='black', which='major', axis='x')
 
     minor_xticks = np.arange(0, 50000, 1000)
-    major_xticks = np.arange(0, 50000, 5000)
+    major_xticks = np.arange(0, 50000, 10000)
     ax.set_xticks(major_xticks)
     ax.set_xticks(minor_xticks, minor=True)
 
-    ax.set_xlabel('Number of Properties')
-    ax.set_ylabel('Quadrant')
-    ax.set_title('Number of Properties by Quadrant')
+    ax.set_xlabel('Number of Properties', fontsize=12)
+    ax.set_ylabel('Quadrant', fontsize=12)
+    ax.set_title('Number of Properties by Quadrant', fontsize=12)
 
     fig.savefig('plots/num_properties_by_quadrant.pdf')
     fig.savefig('plots/num_properties_by_quadrant.png', dpi=300)
@@ -213,18 +212,13 @@ def plot_price_by_coordinate(df: pd.DataFrame):
         final_df = final_df.append(new_series)
 
     plot: 'FramePlotMethods' = final_df.plot.scatter(
-        x='LONGITUDE',
-        y='LATITUDE',
-        s=0.5,
-        c='PRICE ($)',
-        colormap='inferno',
-        marker='H',
-        norm=LogNorm()
-    )
+        x='LONGITUDE', y='LATITUDE', s=0.5,
+        c='PRICE ($)', colormap='inferno', marker='H',
+        norm=LogNorm(), fontsize=12, figsize=(8, 5))
 
     plot.set_axisbelow(True)
     plot.grid(linestyle='--', linewidth='0.5', color='black', which='major')
-    plot.set_title('Mean Price, by Coordinate')
+    plot.set_title('Mean Price, by Coordinate', fontsize=12)
     plt.minorticks_on()
 
     fig = plot.get_figure()
@@ -258,18 +252,13 @@ def plot_count_by_coordinate(df: pd.DataFrame):
         final_df = final_df.append(new_series)
 
     plot: 'FramePlotMethods' = final_df.plot.scatter(
-        x='LONGITUDE',
-        y='LATITUDE',
-        s=0.5,
-        c='Number of Properties',
-        colormap='viridis',
-        marker='H',
-        norm=LogNorm()
-    )
+        x='LONGITUDE', y='LATITUDE', s=0.5,
+        c='Number of Properties', colormap='viridis', marker='H',
+        norm=LogNorm(), fontsize=12, figsize=(8, 5))
 
     plot.set_axisbelow(True)
     plot.grid(linestyle='--', linewidth='0.5', color='black', which='both')
-    plot.set_title('Number of Properties, by Coordinate')
+    plot.set_title('Number of Properties, by Coordinate', fontsize=12)
 
     fig = plot.get_figure()
     fig.savefig('plots/num_properties_by_coordinate.pdf')
@@ -286,19 +275,15 @@ def compute_correlation_price_vs_longitude(df):
     pear = pearsonr(lons, prices)
     print(f'pearsonr: {pear}')
 
-    plot = subset.plot.scatter(
-        x='LONGITUDE',
-        y='PRICE',
-        s=5
-    )
+    plot = subset.plot.scatter(x='LONGITUDE', y='PRICE', s=5)
 
     plot.minorticks_on()
     plot.set_axisbelow(True)
     plot.grid(linestyle='--', linewidth='0.5', color='black', which='major', axis='both')
 
-    plot.set_title('Longitude vs Price')
-    plot.set_xlabel('Longitude')
-    plot.set_ylabel('Price ($)')
+    plot.set_title('Longitude vs Price', fontsize=12)
+    plot.set_xlabel('Longitude', fontsize=12)
+    plot.set_ylabel('Price ($)', fontsize=12)
 
     fig = plot.get_figure()
     fig.savefig('plots/longitude_vs_price.pdf')
@@ -313,18 +298,12 @@ def plots_by_build_date(df):
         subset = df[['AYB', 'PRICE']].dropna().groupby('AYB').agg('mean').sort_values('AYB')
         subset['Build Date'] = subset.index
 
-        plot = subset.plot.line(
-            x='Build Date',
-            y='PRICE',
-            figsize=(9, 5),
-            title='Build Date vs Mean Sale Price',
-            grid=True,
-            legend=True
-        )
+        plot = subset.plot.line(x='Build Date', y='PRICE', title='Build Date vs Mean Sale Price', grid=True,
+                                legend=True)
         plot.minorticks_on()
 
-        plot.set_ylabel('Price ($) (in 10-millions)')
-        plot.set_xlabel('Build Year')
+        plot.set_ylabel('Price ($) (in 10-millions)', fontsize=12)
+        plot.set_xlabel('Build Year', fontsize=12)
         fig = plot.get_figure()
 
         fig.savefig('plots/price_over_time_by_build_date.pdf')
@@ -336,10 +315,16 @@ def plots_by_build_date(df):
         df_age1 = df1[['AYB', 'PRICE']].groupby('AYB').agg('mean').sort_values('AYB')
         df_age2 = df_age1.drop(df_age1.index[[0, 1]])
         df_age2.reset_index()
-        hst2 = df_age2.plot()
-        hst2.set_xlabel("Year")
-        hst2.set_title('1980-2000')
-        fig = hst2.get_figure()
+        plot = df_age2.plot(grid=True, title='Prices from 1980 to 2000', fontsize=12)
+
+        plot.minorticks_on()
+        plot.set_xlabel('Year', fontsize=12)
+        plot.set_ylabel('Price', fontsize=12)
+
+        plot.set_xticks(np.arange(1980, 2001, 5))
+        plot.set_xticks(np.arange(1980, 2001, 1), minor=True)
+
+        fig = plot.get_figure()
         fig.savefig('plots/price_over_time_by_build_date_1980-2000.png', dpi=300)
         fig.savefig('plots/price_over_time_by_build_date_1980-2000.pdf')
         plt.show()
@@ -354,9 +339,9 @@ def plots_price_frequencies(df):
 
         plot = df_age.PRICE.hist()
 
-        plot.set_xlabel('Price')
-        plot.set_title('Price frequency in general')
-        plot.set_ylabel('Number of properties')
+        plot.set_title('Price Histogram', fontsize=12)
+        plot.set_xlabel('Price ($)', fontsize=12)
+        plot.set_ylabel('Number of Properties', fontsize=12)
 
         fig = plot.get_figure()
         fig.savefig('plots/price_freq.png', dpi=300)
@@ -369,11 +354,10 @@ def plots_price_frequencies(df):
         df_age2 = df_age1.drop(df_age1.index[[0, 1]])
         df_age3 = df_age2.reset_index()
 
-        plot = df_age3.PRICE.hist(color=(0.5, 0.1, 0.5, 0.6))
-
-        plot.set_xlabel('Price')
-        plot.set_title("Price frequency 1980-2000")
-        plot.set_ylabel('Number of properties')
+        plot = df_age3.PRICE.hist(color=(0.5, 0.1, 0.5, 0.6), grid=True)
+        plot.set_title('Price Histogram from 1980 to 2000', fontsize=12)
+        plot.set_xlabel('Price ($)', fontsize=12)
+        plot.set_ylabel('Number of Properties', fontsize=12)
 
         fig = plot.get_figure()
         fig.savefig('plots/price_freq80.png', dpi=300)
@@ -386,32 +370,18 @@ def plots_price_frequencies(df):
 
 def price_vs_grade_and_condition(full_df):
     grade_to_num = {
-        'No Data': 0,
-        'Low Quality': 1,
-        'Fair Quality': 2,
-        'Average': 3,
-        'Above Average': 4,
-        'Good Quality': 5,
-        'Very Good': 6,
-        'Superior': 7,
-        'Excellent': 8,
-        'Exceptional-A': 9,
-        'Exceptional-B': 10,
-        'Exceptional-C': 11,
-        'Exceptional-D': 12
-    }
+        'No Data': 0, 'Low Quality': 1, 'Fair Quality': 2,
+        'Average': 3, 'Above Average': 4, 'Good Quality': 5,
+        'Very Good': 6, 'Superior': 7, 'Excellent': 8,
+        'Exceptional-A': 9, 'Exceptional-B': 10, 'Exceptional-C': 11,
+        'Exceptional-D': 12}
 
     condition_to_num = {
-        'Default': 0,
-        'Poor': 1,
-        'Fair': 2,
-        'Average': 3,
-        'Good': 4,
-        'Very Good': 5,
-        'Excellent': 6
-    }
+        'Default': 0, 'Poor': 1, 'Fair': 2,
+        'Average': 3, 'Good': 4, 'Very Good': 5,
+        'Excellent': 6}
 
-    def correlate(x_col, test_type='pearson'):
+    def correlate(x_col):
         """
         Correlates `x_col` with PRICE, using `test_type` as the correlation test
         """
@@ -429,15 +399,14 @@ def price_vs_grade_and_condition(full_df):
         plot = subset.plot.scatter(
             x=column,
             y='PRICE',
-            title=f'Price vs {column}',
-            figsize=(12, 8)
+            title=f'Price vs {column}'
         )
         plot.minorticks_on()
         plot.set_axisbelow(True)
         plot.grid(linestyle='--', linewidth='0.5', color='black', which='major')
 
-        plot.set_xlabel(column)
-        plot.set_ylabel('Price ($)')
+        plot.set_xlabel(column, fontsize=12)
+        plot.set_ylabel('Price ($)', fontsize=12)
 
         fig = plot.get_figure()
         fig.savefig(f'plots/{column}_vs_price.pdf')
@@ -464,18 +433,13 @@ def price_vs_grade_and_condition(full_df):
             }, name=col))
 
         final_df.sort_values(by='MEAN_PRICE', inplace=True)
-        plot = final_df.plot.barh(
-            x=column,
-            y='MEAN_PRICE',
-            figsize=(12, 8),
-            title=f'Mean Price by {column}',
-            grid=True
-        )
+        plot = final_df.plot.barh(x=column, y='MEAN_PRICE', title=f'Mean Price by {column}', grid=True,
+                                  fontsize=12, figsize=(13, 5))
         plot.minorticks_on()
         plot.set_axisbelow(True)
         plot.grid(linestyle='--', linewidth='0.5', color='black', which='major')
 
-        plot.set_xlabel('Mean Price ($)')
+        plot.set_xlabel('Mean Price ($)', fontsize=12)
 
         fig = plot.get_figure()
         fig.savefig(f'plots/mean_price_by_{column}.pdf')
